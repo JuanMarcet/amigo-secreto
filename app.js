fbba47ed5047be183d2cf65c596dea1dd0df568f
Juan Marcet <juanmarcet06@gmail.com>
2025-02-16 19:47:29 -0500

    evitar nombre repetido

--- app.js
+++ app.js
@@ -4,21 +4,32 @@ let arrJugadores = [];
 
 
 
+
 function agregarAmigo(){
     nombreJugador = document.getElementById('amigo').value;
     // se comprueba que la textbox no esté vacía, con el metodo .trim() se eliminan los espacios en blanco al inicio y al final de la cadena de texto
     if (nombreJugador.trim() == ""){ 
         alert('No se la ingresado un nombre en la caja de texto.');
     } else{
-        // agrega el nuevo nombre al arrar de jugadores
-        arrJugadores.push(nombreJugador.trim());
+        if (arrJugadores.includes(nombreJugador)){
+            alert(`${nombreJugador} ya se encuentra registrado, si es una persona distinta añada el apellido.`);
+        }else{
+            // agrega el nuevo nombre al arrar de jugadores
+            arrJugadores.push(nombreJugador.trim());
 
-        let listaAmigos = document.getElementById('listaAmigos'); // se selecciona la lista por el ID
-        let  nuevoAmigo = document.createElement('li'); // se crea un nuevo elemento a la lista 
-        nuevoAmigo.textContent = nombreJugador; //  se le añade el valor o texto al nuevo elemento de la lista
-        listaAmigos.appendChild(nuevoAmigo); // con el atributo se añade el elemento a la lista principal
+            let listaAmigos = document.getElementById('listaAmigos'); // se selecciona la lista por el ID
+            let  nuevoAmigo = document.createElement('li'); // se crea un nuevo elemento a la lista 
+            nuevoAmigo.textContent = nombreJugador; //  se le añade el valor o texto al nuevo elemento de la lista
+            listaAmigos.appendChild(nuevoAmigo); // con el atributo se añade el elemento a la lista principal
+        }
+        document.getElementById('amigo').value = ''; // se coloca la textbox vacía
+        console.log (arrJugadores);
+        }
     }
-    document.getElementById('amigo').value = ''; // se coloca la textbox vacía
-    console.log (arrJugadores);
-}
+
+document.getElementById('amigo').addEventListener('keydown', function (event ){
+    if (event.key === 'Enter'){
+        agregarAmigo();
+    }
+});
 
